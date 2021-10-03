e1e6766c2d70b4fe3ed6159107d452677cddd2e4
vgenij-asiliev <vasiliev.evgenij.aleksandrovich@gmail.com>
2021-10-03 23:48:41 +0300

    new react project

---
+++ src/Table.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Table extends Component {
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Job</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Mac</td>
+            <td>Bouncer</td>
+          </tr>
+          <tr>
+            <td>Dennis</td>
+            <td>Bartender</td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default Table;
